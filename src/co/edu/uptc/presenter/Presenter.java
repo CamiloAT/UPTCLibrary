4a2643cc610b5ab9a15aff3ee22d1b46e91f7041
Juan Jose Albornoz <juan.albornoz@uptc.edu.co>
2023-09-23 21:53:37 -0500

    se añadieron exepciones a vista AddBook

--- src/co/edu/uptc/presenter/Presenter.java
+++ src/co/edu/uptc/presenter/Presenter.java
@@ -1,15 +1,14 @@
 package co.edu.uptc.presenter;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
 import co.edu.uptc.model.Author;
 import co.edu.uptc.model.Book;
 import co.edu.uptc.model.TreeAVL;
+import co.edu.uptc.view.ExceptionPanel;
 import co.edu.uptc.view.MyFrame;
 
-public class Presenter implements ActionListener{
+public class Presenter implements ActionListener {
 
 	private MyFrame frame;
 	private TreeAVL tree;
@@ -17,6 +16,7 @@ public class Presenter implements ActionListener{
 	public Presenter() {
 		frame = new MyFrame(this);
 		tree = new TreeAVL();
+
 		this.addBooks();
 		this.fillComboBoxes();
 	}
@@ -24,12 +24,26 @@ public class Presenter implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent event) {
 		String source = event.getActionCommand();
+		ExceptionPanel exceptionPanel;
 		switch (source) {
 		case "addBook":
-			tree.insertar(new Book(frame.getSpaceNameBook(), Integer.parseInt(frame.getSpaceCode()), frame.getSpaceVolume(), frame.getSpaceEditorial(), frame.getPalabraCombo(), new Author(frame.getSpaceNameAuthor(), frame.getSpaceLastName(), frame.getDescriptionTextArea())));
-			frame.setConfirAdd(true);
-			break;
+			try {
+				if (Integer.parseInt(frame.getSpaceCode()) < 0)throw new Exception();
+				if(frame.getSpaceNameBook().isEmpty()|| frame.getSpaceCode().isEmpty()||frame.getSpaceVolume().isEmpty()||frame.getSpaceEditorial().isEmpty()||frame.getSpaceNameAuthor().isEmpty()||frame.getSpaceLastName().isEmpty()||frame.getDescriptionTextArea().isEmpty()) {throw new Exception();}
+				tree.insertar(new Book(frame.getSpaceNameBook(), Integer.parseInt(frame.getSpaceCode()),
+						frame.getSpaceVolume(), frame.getSpaceEditorial(), frame.getPalabraCombo(), new Author(
+								frame.getSpaceNameAuthor(), frame.getSpaceLastName(), frame.getDescriptionTextArea())));
+				frame.setConfirAdd(true);
 
+			} catch (NumberFormatException e) {
+				exceptionPanel = new ExceptionPanel("Ingresa un ISBN correcto");
+				frame.setConfirAdd(false);
+			} catch (Exception e) {
+//				e.printStackTrace();
+				exceptionPanel = new ExceptionPanel("Ingresa informacion correcta");
+				frame.setConfirAdd(false);
+			}
+			break;
 		case "addMenu":
 			frame.showAdd();
 			break;
@@ -53,67 +67,74 @@ public class Presenter implements ActionListener{
 		case "delete":
 			this.deleteBook();
 			break;
-			
+
 		case "searchBook":
 			this.searchBook();
 			break;
-			
+
 		case "listBooks":
 			this.listBook();
 			break;
 		}
 	}
-	
+
 	private void fillComboBoxes() {
 		frame.fillSearchComboBox(tree.getSedes());
 		frame.fillDeleteComboBox(tree.getSedes());
 		frame.fillListComboBox(tree.getSedes());
 		frame.fillCbSedes(tree.getSedes());
 	}
-	
+
 	private void addBooks() {
-		tree.insertar(new Book("Camilo", 1, "Volumen 4.5", "Ramiriqui's fundation", "Tunja, Campus: Central Jorge Palacios Preciado", new Author("Carlos", "Parra", "Se suicido")));
-		tree.insertar(new Book("Años de soledad", 2, "Volumen 4.5", "Ramiriqui's fundation", "Tunja, Campus: Central Jorge Palacios Preciado", new Author("Carlos", "Parra", "Se suicido")));
-		tree.insertar(new Book("Casa", 3, "Volumen 4.5", "Ramiriqui's fundation", "Tunja, Campus: Central Jorge Palacios Preciado", new Author("Carlos", "Parra", "Se suicido")));
-		tree.insertar(new Book("Aerografo", 4, "Volumen 4.5", "Ramiriqui's fundation", "Tunja, Campus: Central Jorge Palacios Preciado", new Author("Carlos", "Parra", "Se suicido")));
+		tree.insertar(new Book("Camilo", 1, "Volumen 4.5", "Ramiriqui's fundation",
+				"Tunja, Campus: Central Jorge Palacios Preciado", new Author("Carlos", "Parra", "Se suicido")));
+		tree.insertar(new Book("Años de soledad", 2, "Volumen 4.5", "Ramiriqui's fundation",
+				"Tunja, Campus: Central Jorge Palacios Preciado", new Author("Carlos", "Parra", "Se suicido")));
+		tree.insertar(new Book("Casa", 3, "Volumen 4.5", "Ramiriqui's fundation",
+				"Tunja, Campus: Central Jorge Palacios Preciado", new Author("Carlos", "Parra", "Se suicido")));
+		tree.insertar(new Book("Aerografo", 4, "Volumen 4.5", "Ramiriqui's fundation",
+				"Tunja, Campus: Central Jorge Palacios Preciado", new Author("Carlos", "Parra", "Se suicido")));
 	}
-	
-	private void deleteBook(){
-		if(frame.getWoldComboDelete().equals("Ninguno") & tree.searchByISBN(frame.getIsbnDelete()) != null){
+
+	private void deleteBook() {
+		if (frame.getWoldComboDelete().equals("Ninguno") & tree.searchByISBN(frame.getIsbnDelete()) != null) {
 			tree.delete(tree.searchByISBN(frame.getIsbnDelete()));
 			frame.setConfirDelete(true);
 
-		} else if((!frame.getWoldComboDelete().equals("Ninguno")) & (tree.searchBookBySede(tree.searchBySede(frame.getWoldComboDelete()), frame.getIsbnDelete()).getISBN() == frame.getIsbnDelete())){
-			System.out.println(tree.searchBookBySede(tree.searchBySede(frame.getWoldComboDelete()), frame.getIsbnDelete()) != null);
+		} else if ((!frame.getWoldComboDelete().equals("Ninguno"))
+				& (tree.searchBookBySede(tree.searchBySede(frame.getWoldComboDelete()), frame.getIsbnDelete())
+						.getISBN() == frame.getIsbnDelete())) {
+			System.out.println(tree.searchBookBySede(tree.searchBySede(frame.getWoldComboDelete()),
+					frame.getIsbnDelete()) != null);
 			tree.delete(tree.searchBookBySede(tree.searchBySede(frame.getWoldComboDelete()), frame.getIsbnDelete()));
 			frame.setConfirDelete(true);
 
 		} else {
 			frame.setConfirDelete(false);
 		}
-			fillComboBoxes();
+		fillComboBoxes();
 	}
 
 	private void searchBook() {
-		if(!frame.getIsbnToSearch().equals("")) {
+		if (!frame.getIsbnToSearch().equals("")) {
 			ArrayList<Book> temporal = new ArrayList<>();
 			temporal.add(tree.searchByISBN(Integer.parseInt(frame.getIsbnToSearch())));
 			frame.fillSearchTable(temporal);
-		}else if(!frame.getNameToSearch().equals("")) {
+		} else if (!frame.getNameToSearch().equals("")) {
 			frame.fillSearchTable(tree.searchByName(frame.getNameToSearch()));
-		}else if(!frame.getWorldSearchComboBox().equals("Ninguno")) {
+		} else if (!frame.getWorldSearchComboBox().equals("Ninguno")) {
 			frame.fillSearchTable(tree.searchBySede(frame.getWorldSearchComboBox()));
 		}
 	}
-	
+
 	private void listBook() {
-		if(frame.getWorldListComboBox().equals("Ninguno")) {
+		if (frame.getWorldListComboBox().equals("Ninguno")) {
 			frame.fillListTable(tree.listAllBooks());
-		}else{
+		} else {
 			frame.fillListTable(tree.listBooksBySede(frame.getWorldListComboBox()));
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new Presenter();
 	}
